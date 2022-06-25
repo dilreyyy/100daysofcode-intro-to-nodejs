e1befa494bfce5e9fc50bf8e923438278ce00d10
Its Ya Boi Del Rey <delreeyyy@gmail.com>
2022-06-25 22:51:22 +0800

    Retrieve and display data from json file

--- demo.js
+++ demo.js
@@ -26,17 +26,21 @@ const express = require('express');
 
 const app = express();
 
+//TO HANDLE ALL REQUESTS 
 app.use(express.urlencoded({extended: false}));
 
+//ROUTE FOR CURRENTTIME PAGE
 app.get('/currenttime', function(req, res){
     res.send("<h1>" +new Date().toISOString() +"</h1>");
 });
 
+//HOME PAGE ROUTE 
 app.get('/', function(req, res){
     // res.send("<h1>Hello, world!");
     res.send("<form action='/store-user' method='POST'><label>Username: </label><input type='text' name='username'/><button>Submit</button></form>")
 });
 
+//ROUTE FOR ENTER USERNAME PAGE
 app.post('/store-user', function(req, res){
     const userName = req.body.username;
 
@@ -50,6 +54,24 @@ app.post('/store-user', function(req, res){
 
     // console.log(userName);
     res.send("<h3>User stored!");
+});
+
+//DISPLAY USERNAME ROUTE 
+app.get('/display-users', function(req, res){
+    const filePath = path.join(__dirname, 'data', 'users.json');
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData);
+
+    let displayUsers = "<ul>";
+
+    for (const user of existingUsers){
+        displayUsers += "<li>" +user +"</li>"; 
+    }
+
+    displayUsers += "</ul>";
+
+    res.send(displayUsers);
+
 });
 
 app.listen(3000);
